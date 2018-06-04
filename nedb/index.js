d0f7a609bf68daf32f5f87e775fcdefb21aae430
Garrett Gu <garrettgu777@gmail.com>
2018-06-04 16:58:08 -0500

    default colors yay

--- nedb/index.js
+++ nedb/index.js
@@ -82,6 +82,7 @@ function randomColor() {
 function compileChart( result ) {
     var {term, subj, num, sect, desc, prof, grades} = result;
     var total = Object.values(grades).reduce((a, b) => a+parseInt(b), 0);
+    var colors = ['#30c737', '#93d10d', '#ffe14d', '#ffad33', '#ff704d', '#f518a9', '#a851a8', '#96d529'];
     var myChart = Highcharts.chart('chart', {
         chart: {
           type: 'column'
@@ -127,7 +128,7 @@ function compileChart( result ) {
           data: Object.values(grades).map((num, i) => {
                 return {
                     y: parseInt(num), 
-                    color: randomColor(), 
+                    color: colors[i] || randomColor(), 
                     percentage: num/total*100
                 };
           })
