76d9571b37b4d9421bca0b512335ae6c133cc30c
Jeffrey Wang <j@mywikis.com>
2018-07-07 14:29:57 -0500

    Adding sharing capabilities

--- index.js
+++ index.js
@@ -95,6 +95,21 @@ function randomColor() {
     return color;
 }
 
+// from Stack Overflow https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript/901144#901144
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function shareURL( result ) {
+
+}
+
 function compileChart( result ) {
     var {term, subj, num, sect, desc, prof, grades} = result;
     var total = Object.values(grades).reduce((a, b) => a+parseInt(b), 0);
@@ -150,4 +165,6 @@ function compileChart( result ) {
           })
         }]
     });
+    $('#sharelink').val(window.location.href + '?term=' + encodeURIComponent(term) + '&subj=' + encodeURIComponent(subj) + '&num=' + encodeURIComponent(num) + '&sect=' + encodeURIComponent(sect) + '&desc=' + encodeURIComponent(desc) + '&prof=' + encodeURIComponent(prof) + '&grades=' + encodeURIComponent(btoa(JSON.stringify(grades))));
+    $('#share').show();
 }
