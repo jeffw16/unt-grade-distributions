6b4a98d500cfb1a19c8e2880c32758d485d932ab
Jeffrey Wang <j@mywikis.com>
2019-06-04 18:08:30 -0700

    Spring 2019 data upload

--- parse.php
+++ parse.php
@@ -4,9 +4,9 @@
  * @author Jeffrey Wang (@jeffw16)
 */
 
-$csv_filename = 'UNT_Grade_Distributions-Fall2018';
-$json_filename = 'fall2018';
-$term = '2018 Fall';
+$csv_filename = 'UNT_Grade_Distributions-Spring2019';
+$json_filename = 'spring2019';
+$term = '2019 Spring';
 
 $res_csv = fopen( 'static/' . $csv_filename . '.csv', 'r' );
 $res_json = fopen( 'static/' . $json_filename . '.json', 'w' );
@@ -23,11 +23,11 @@ $narr = array();
 for ( $i = 1; $i < count( $arr ); $i++ ) {
   $linearr = array();
   $linearr['term'] = $term;
-  $linearr['prof'] = utf8_encode($arr[$i][0]); // primary professor
-  $linearr['subj'] = utf8_encode($arr[$i][3]);
-  $linearr['num'] = utf8_encode($arr[$i][4]);
-  $linearr['sect'] = utf8_encode($arr[$i][5]);
-  $linearr['desc'] = utf8_encode($arr[$i][6]);
+  $linearr['prof'] = utf8_encode($arr[$i][7]); // primary professor
+  $linearr['subj'] = utf8_encode($arr[$i][2]);
+  $linearr['num'] = utf8_encode($arr[$i][3]);
+  $linearr['sect'] = utf8_encode($arr[$i][4]);
+  $linearr['desc'] = utf8_encode($arr[$i][5]);
   if ( $i > 0 && sizeof($narr) > 0 && $arr[$i - 1][3] == $linearr['subj'] && $arr[$i - 1][4] == $linearr['num'] && $arr[$i][5] == $linearr['sect'] ) {
     $narr[sizeof($narr) - 1]['grades'][utf8_encode($arr[$i][8])] = utf8_encode($arr[$i][9]);
   } else {
