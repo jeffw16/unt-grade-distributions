dcfd63b2124b0e55d60ba32fbcd2356ce847fc7a
Jordan Hamilton <JordanHamilton@my.unt.edu>
2019-11-15 18:32:16 -0800

    done messing around for today. Site is functional.

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ function instantFind() {
     }
 }
 
+
 function findClasses() {
     var select_result = document.getElementById('select_result');
     select_result.innerHTML = "loading...";
@@ -48,32 +49,9 @@ function findClasses() {
         window.loading = true;
         return;
     }
-	
-	if (document.getElementById('instructor').innerHTML === ""){
     window.db.find(generateQuery())
-        .sort({ term: -1, num: 1, prof: 2,  sect: 5, subj: 3})
-        //.sort({ term: -1, prof: 2, sect: 5, subj: 3, num: 4 })
-		.exec((err, docs) => {
-            // console.log(docs);
-            if(docs.length == 0){
-                select_result.innerHTML = "No results were found. Try modifying your query. While we strive to keep a complete record, there may be some deficiencies in what the registrar provides us.";
-            } else {
-                select_result.innerHTML = ""; // clears all section results
-                for ( var i = 0; i < docs.length; i++ ) {
-                    var item = document.createElement('li'); // list item
-                    let entry = docs[i]; // use "let" instead of "var" so variable scope is local
-                    item.appendChild(document.createTextNode(formatResult(entry)));
-                    item.style.color = "#2f843e";
-                    item.style.cursor = 'pointer';
-                    item.onclick = function(){ compileChart(entry); };
-                    document.getElementById('select_result').append(item);
-                }
-            }
-        });
-} else {
-	window.db.find(generateQuery())
         .sort({ term: -1, prof: 2, sect: 5, subj: 3, num: 4 })
-		.exec((err, docs) => {
+        .exec((err, docs) => {
             // console.log(docs);
             if(docs.length == 0){
                 select_result.innerHTML = "No results were found. Try modifying your query. While we strive to keep a complete record, there may be some deficiencies in what the registrar provides us.";
@@ -90,12 +68,16 @@ function findClasses() {
                 }
             }
         });
-  }
 }
+
+				//.sort({ term: -1, prof: 2, sect: 5, subj: 3, num: 4 }) first statement
+				//.sort({ term: -1, prof: -1, sect: 5, subj: 3, num: 4 })  else statement without professor
+
+
+
 
 function generateQuery() {
   var query = {
-        // term: document.getElementById('semester').value,
         subj: generateContainsRegex(document.getElementById('subject').value),
         num: generateContainsRegex(document.getElementById('course').value),
         desc: generateContainsRegex(document.getElementById('desc').value.toUpperCase()),
