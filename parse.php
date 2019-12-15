74f7e7ed0d1bf34ffa6c1a8af1b6a937699acb9b
Jacob Preston <prestja@gmail.com>
2019-12-14 18:49:54 -0600

    Reformatted parse.php and included cleanup procedures for sections that have no real grades

--- parse.php
+++ parse.php
@@ -13,35 +13,50 @@ $res_json = fopen( 'static/' . $json_filename . '.json', 'w' );
 
 $arr = array();
 $csv_line = array();
-while ( ( $csv_line = fgetcsv( $res_csv ) ) !== false ) {
-  $arr[] = $csv_line;
-}
-// print_r($arr);
 $narr = array();
 
+while ( ( $csv_line = fgetcsv( $res_csv ) ) !== false ) 
+{
+	$arr[] = $csv_line;
+}
+
 // start at 1st row because the first row contains introductory background info
-for ( $i = 1; $i < count( $arr ); $i++ ) {
-  $linearr = array();
-  $linearr['term'] = $term;
-  $linearr['prof'] = utf8_encode($arr[$i][7]); // primary professor
-  $linearr['subj'] = utf8_encode($arr[$i][2]);
-  $linearr['num'] = utf8_encode($arr[$i][3]);
-  $linearr['sect'] = utf8_encode($arr[$i][4]);
-  $linearr['desc'] = utf8_encode($arr[$i][5]);
-  if ( $i > 0 && sizeof($narr) > 0 && $arr[$i - 1][2] == $linearr['subj'] && $arr[$i - 1][3] == $linearr['num'] && $arr[$i - 1][4] == $linearr['sect'] ) {
-    $narr[sizeof($narr) - 1]['grades'][utf8_encode($arr[$i][9])] = utf8_encode($arr[$i][10]);
-  } else {
-    $linearr['grades'] = array();
-    $linearr['grades'][utf8_encode($arr[$i][9])] = utf8_encode($arr[$i][10]);
-    $narr[] = $linearr;
-  }
+for ( $i = 1; $i < count( $arr ); $i++ ) 
+{
+	$linearr = array();
+	$linearr['term'] = $term;
+	$linearr['prof'] = utf8_encode($arr[$i][7]); // primary professor
+	$linearr['subj'] = utf8_encode($arr[$i][2]);
+	$linearr['num'] = utf8_encode($arr[$i][3]);
+	$linearr['sect'] = utf8_encode($arr[$i][4]);
+	$linearr['desc'] = utf8_encode($arr[$i][5]);
+	if ( ($i > 0 && sizeof($narr) > 0 && $arr[$i - 1][2] == $linearr['subj'] && $arr[$i - 1][3] == $linearr['num'] && $arr[$i - 1][4] == $linearr['sect'])) // if section already exists in array
+	{			
+		$narr[sizeof($narr) - 1]['grades'][utf8_encode($arr[$i][9])] = utf8_encode($arr[$i][10]);
+	}	  
+	else // if section does not already exist in the array
+	{
+		$linearr['grades'] = array();
+		$linearr['grades'][utf8_encode($arr[$i][9])] = utf8_encode($arr[$i][10]);
+		$narr[] = $linearr;
+	}
+}
+
+for ( $i = 1; $i < count($narr); $i++) // eliminates entries in which the only grades are ungraded, or ''
+{		
+	if (count($narr[$i]['grades']) == 1 && $narr[$i]['grades'][''] > 0) 
+	{
+		unset($narr[$i]); // remove the element
+	}
 }
 
 $jsonstr = json_encode( $narr, JSON_UNESCAPED_UNICODE );
-
-if ( $jsonstr === false ) {
-  echo "Error!";
-  echo json_last_error();
-} else {
-  fwrite( $res_json,  $jsonstr);
+if ( $jsonstr === false ) 
+{
+	echo "Error!";
+	echo json_last_error();
+} 
+else 
+{
+	fwrite( $res_json,  $jsonstr);
 }
