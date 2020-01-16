db307e4729143202527a07866ee771480b89af5b
Jordan Hamilton <JordanHamilton@my.unt.edu>
2020-01-15 21:00:51 -0800

    added favicon and dropdown that needs centering

--- index.js
+++ index.js
@@ -17,6 +17,25 @@ document.addEventListener("DOMContentLoaded", function() {
 	};
 	xhr.send();
   });
+
+ //dropdown JS below
+  function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+	  var dropdowns = document.getElementsByClassName("dropdown-content");
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  } 
   
   function escapeRegex(str) {
 	return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
